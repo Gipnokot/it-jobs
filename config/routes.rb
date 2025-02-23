6c86fb77d461e11e6e795546ffe2e1e23e6e9272
Gipnokot <shootinputin007@gmail.com>
2025-02-23 17:43:50 +0500

    rubocop autocorrect

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "seed/run"
   resources :vacancies, only: [ :index, :show, :new, :create ]
   root "vacancies#index"
-  post 'seed/run', to: 'seed#run'
+  post "seed/run", to: "seed#run"
 end
