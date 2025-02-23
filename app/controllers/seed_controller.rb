0afd0a0d821353bae44e940aaac146919c49603c
Gipnokot <shootinputin007@gmail.com>
2025-02-23 18:11:08 +0500

    fix

--- app/controllers/seed_controller.rb
+++ app/controllers/seed_controller.rb
@@ -1,23 +1,11 @@
-# app/controllers/seed_controller.rb
-
-require 'rake'
-
 class SeedController < ApplicationController
+  skip_before_action :verify_authenticity_token  # Это позволяет отправлять запросы без CSRF токена
+
   def run
-    begin
-      # Инициализация объекта Rake
-      rake = Rake.application
-      rake.init
-      rake.load_rakefile
-
-      # Вызов задачи db:seed
-      Rake::Task['db:seed'].invoke
-
-      # Возвращаем успешный ответ
-      render json: { message: 'Seed complete!' }, status: :ok
-    rescue => e
-      # Если произошла ошибка, возвращаем ошибку
-      render json: { error: e.message }, status: :unprocessable_entity
-    end
+    # Ваш код для сидирования данных
+    Rails.application.load_seed
+    render json: { message: "Seeding completed successfully" }, status: :ok
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
   end
 end
